3622b1593b9bbfccd5fb4ad8fd0fbc36220daaf7
Alyne <a.alynerezende@gmail.com>
2024-10-11 11:45:41 +0100

    updating json

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/AlyneRezende/my_portfolio/refs/heads/main/src/data/profile.json?token=GHSAT0AAAAAACX4N6KWSFM5IN36JMLOWJA2ZYI7FTA'
+    const url = 'https://raw.githubusercontent.com/AlyneRezende/my_portfolio/refs/heads/main/src/data/profile.json?token=GHSAT0AAAAAACX4N6KWSJDUTVPU4SYMDTSGZYI7WNQ'
     const fetching = await fetch(url)
     return await fetching.json()
 }
