db7db8584081eb355ed0d54896f0e7e77ab9528d
Alyne <a.alynerezende@gmail.com>
2024-10-11 15:29:33 +0100

    almost done

--- src/js/main.js
+++ src/js/main.js
@@ -21,10 +21,45 @@ function updateProfile(profileData) {
     email.href = `mailto:${profileData.email}`
 }
 
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `
+        <li>
+            <img src="${skill.logo}" alt="${skill.name}" title="${skill.name}">
+        </li>
+        `).join('')
+}
+
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softSkills')
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
+function updateLanguages(profileData) {
+    const languages = document.getElementById('profile.languages')
+    languages.innerHTML = profileData.languages.map(language => `
+        <li>${language}</li>
+        `).join('')
+}
+
+function updateProjects(profileData) {
+    const projects = document.getElementById('profile.projects')
+    projects.innerHTML = profileData.projects.map(project => {
+        return `
+        <li>
+            <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
+            <p>${project.description}</p>
+            <a href="${project.url}" target="_blank">${project.url}</a>
+        </li>
+        `
+    }).join('')
+}
 
 (async () => {
     const profileData = await fetchProfileData()
-    console.log(profileData)
     updateProfile(profileData)
-
+    updateHardSkills(profileData)
+    updateSoftSkills(profileData)
+    updateLanguages(profileData)
+    updateProjects(profileData)
 })()
